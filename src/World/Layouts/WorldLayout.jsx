25fd9512d3b0244011530a341e1190fa21055fe9
Odda Kussa <oddakussa@outlook.com>
2023-04-29 17:36:34 +0300

    Initial commit

---
+++ src/World/Layouts/WorldLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+
+const WorldLayout = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default WorldLayout
