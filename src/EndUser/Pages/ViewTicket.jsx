88290c68a75d9c006c0bd24dcd7da50cb24588c1
Odda Kussa <oddakussa@outlook.com>
2023-05-10 00:12:36 +0300

    UI fix on view ticket

--- src/EndUser/Pages/ViewTicket.jsx
+++ src/EndUser/Pages/ViewTicket.jsx
@@ -1,43 +1,47 @@
 
 const ViewTicket = () => {
     return (
-        <div className="mt-10">
-            <div className="chat chat-start">
-                <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                        <img src="/logo512.png" />
+        <div className="card bg-base-100 shadow-xl mt-10">
+            <div className="card-body">
+                <div>
+                    <div className="chat chat-start">
+                        <div className="chat-image avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="/logo512.png" />
+                            </div>
+                        </div>
+                        <div className="chat-header">
+                            Obi-Wan Kenobi
+                            <time className="text-xs opacity-50">12:45</time>
+                        </div>
+                        <div className="chat-bubble">
+                            That's never been done in the history of the Jedi. It's insulting!
+                            It was said that you would, destroy the Sith, not join them.
+                            It was said that you would, destroy the Sith, not join them.
+                        </div>
+                        <div className="chat-footer opacity-50">
+                            Delivered
+                        </div>
                     </div>
-                </div>
-                <div className="chat-header">
-                    Obi-Wan Kenobi
-                    <time className="text-xs opacity-50">12:45</time>
-                </div>
-                <div className="chat-bubble">
-                    That's never been done in the history of the Jedi. It's insulting!
-                    It was said that you would, destroy the Sith, not join them.
-                    It was said that you would, destroy the Sith, not join them.
-                </div>
-                <div className="chat-footer opacity-50">
-                    Delivered
-                </div>
-            </div>
-            <div className="chat chat-end">
-                <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                        <img src="/default-avatar.png" />
+                    <div className="chat chat-end">
+                        <div className="chat-image avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="/default-avatar.png" />
+                            </div>
+                        </div>
+                        <div className="chat-header">
+                            Anakin
+                            <time className="text-xs opacity-50">12:46</time>
+                        </div>
+                        <div className="chat-bubble">
+                            That's never been done in the history of the Jedi. It's insulting!
+                            It was said that you would, destroy the Sith, not join them.
+                            It was said that you would, destroy the Sith, not join them.
+                        </div>
+                        <div className="chat-footer opacity-50">
+                            Seen at 12:46
+                        </div>
                     </div>
-                </div>
-                <div className="chat-header">
-                    Anakin
-                    <time className="text-xs opacity-50">12:46</time>
-                </div>
-                <div className="chat-bubble">
-                    That's never been done in the history of the Jedi. It's insulting!
-                    It was said that you would, destroy the Sith, not join them.
-                    It was said that you would, destroy the Sith, not join them.
-                </div>
-                <div className="chat-footer opacity-50">
-                    Seen at 12:46
                 </div>
             </div>
         </div>
