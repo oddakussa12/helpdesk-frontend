5470ca6e9458227626ee50b850b17d00a5c0daae
Odda Kussa <oddakussa@outlook.com>
2023-05-12 23:01:50 +0300

    Update support profile page

--- src/Support/Layouts/SupportLayout.jsx
+++ src/Support/Layouts/SupportLayout.jsx
@@ -16,7 +16,7 @@ const SupportLayout = () => {
                         height: '400px'
                     }}>
                         <div className="flex flex-row flex-wrap">
-                            <div className="sm:basis-1/2 md:basis-1/4 lg:basis-1/4">
+                            <div className="basis-1/2 md:basis-1/4 lg:basis-1/4">
                                 <div className="px-3 py-2">
                                     <div className="card bg-base-100 shadow-xl">
                                         <div className="card-body">
@@ -41,7 +41,7 @@ const SupportLayout = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="sm:basis-1/2 md:basis-1/4 lg:basis-1/4">
+                            <div className="basis-1/2 md:basis-1/4 lg:basis-1/4">
                                 <div className="px-3 py-2">
                                     <div className="card bg-base-100 shadow-xl">
                                         <div className="card-body">
@@ -66,7 +66,7 @@ const SupportLayout = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="sm:basis-1/2 md:basis-1/4 lg:basis-1/4">
+                            <div className="basis-1/2 md:basis-1/4 lg:basis-1/4">
                                 <div className="px-3 py-2">
                                     <div className="card bg-base-100 shadow-xl">
                                         <div className="card-body">
@@ -91,7 +91,7 @@ const SupportLayout = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="sm:basis-1/2 md:basis-1/4 lg:basis-1/4">
+                            <div className="basis-1/2 md:basis-1/4 lg:basis-1/4">
                                 <div className="px-3 py-2">
                                     <div className="card bg-base-100 shadow-xl">
                                         <div className="card-body">
--- src/Support/Pages/SupportProfile.jsx
+++ src/Support/Pages/SupportProfile.jsx
@@ -6,51 +6,84 @@ const SupportProfile = () => {
         <div className="card-body">
           <h2 className="card-title">Account Management</h2>
           <div className="overflow-x-auto">
-            <div className="flex gap-8">
-              <div className="basis-3/4">
-                <div className="card bg-base-200 text-dark-content" style={{minHeight:'450px'}} >
-                  <div className="card-body items-center text-center">
-                    <div className="avatar flex flex-col justify-center items-center">
-                      <div className="w-24 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2">
-                        <img src="/pp.jpg" />
-                      </div>
-                      <p className="mt-4">Odda Kussa</p>
-                      <small>Support - Level one</small>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+              <div className="card bg-base-200 col-span-2" style={{ minHeight: '450px' }} >
+                <div className="card-body">
+                  <div className="avatar flex flex-col items-center">
+                    <div className="w-24 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2">
+                      <img src="/pp.jpg" />
                     </div>
+                    <p className="mt-4">Odda Kussa</p>
+                    <small>Support - Level one</small>
                   </div>
+                  <form className="mt-2">
+                    <div className="grid grid-cols-2">
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">First name</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-sm" />
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Last name</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-sm" />
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Phone number</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-sm" />
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-sm" />
+                      </div>
+                    </div>
+
+
+
+                    <div className="flex justify-center mt-10">
+                      <button className="btn btn-warning"
+                        style={{ width: '150px', borderRadius: '2px' }}>
+                        Submit
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
-              <div className="basis-1/4">
-                <div className="card w-96 bg-base-200 shadow-sm">
-                  <div className="card-body">
-                    <b>Update your password</b>
-                    <form className="mt-2">
-                      <div className="form-control">
-                        <label className="label">
-                          <span className="label-text">Old password</span>
-                        </label>
-                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
-                      </div>
-                      <div className="form-control">
-                        <label className="label">
-                          <span className="label-text">New password</span>
-                        </label>
-                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
-                      </div>
-                      <div className="form-control">
-                        <label className="label">
-                          <span className="label-text">Confirm password</span>
-                        </label>
-                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
-                      </div>
-                      <div className="flex justify-center mt-10">
-                        <button className="btn btn-warning"
-                          style={{ width: '150px', borderRadius: '2px' }}>
-                          Submit
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+              <div className="card bg-base-200 col-span-1">
+                <div className="card-body text-center items-center">
+                  <b>Update your password</b>
+                  <form className="mt-2">
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Old password</span>
+                      </label>
+                      <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">New password</span>
+                      </label>
+                      <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Confirm password</span>
+                      </label>
+                      <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                    </div>
+                    <div className="flex justify-center mt-10">
+                      <button className="btn btn-warning"
+                        style={{ width: '150px', borderRadius: '2px' }}>
+                        Submit
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
