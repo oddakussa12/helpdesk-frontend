c94b343930d39d78da4c77de851bf8aedbfb7316
Odda Kussa <oddakussa@outlook.com>
2023-06-02 23:21:29 +0300

    Delete faq endpoint integration

--- src/Admin/Pages/Faq/AdminFaq.jsx
+++ src/Admin/Pages/Faq/AdminFaq.jsx
@@ -5,22 +5,28 @@ import useAdminFaqService from "./Api/faq.service"
 import { PencilIcon } from "@heroicons/react/24/solid";
 import { EyeIcon } from "@heroicons/react/24/solid";
 import { TrashIcon } from "@heroicons/react/24/solid";
+import parse from "html-react-parser";
 
 const AdminFaq = () => {
 
-    const { getAllFaqs,
-        createFaq,
-        updateFaq,
-        deleteFaq,
-        approveFaq,
-        declineFaq } = useAdminFaqService();
+    const {
+        getAllFaqs,
+        deleteFaq
+    } = useAdminFaqService();
 
     const [faqs, setFaqs] = useState({});
+    const [selectedItem, setSelectedItem] = useState({});
+
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const handleShowConfirmModal = (faq) => {
+        setSelectedItem(faq);
+        setShowConfirmModal(true);
+    }
+    const handleCloseConfirmModal = () => setShowConfirmModal(false);
 
     const fetchFaqs = async () => {
         try {
             const response = await getAllFaqs();
-            console.log("Faqs", response.data);
             setFaqs(response.data);
         } catch (err) {
             console.log(err);
@@ -33,6 +39,13 @@ const AdminFaq = () => {
 
     return (
         <div className="px-3 mt-10">
+            <ConfirmModal
+                showConfirmModal={showConfirmModal}
+                handleCloseConfirmModal={handleCloseConfirmModal}
+                selectedItem={selectedItem}
+                deleteAction={deleteFaq}
+                fetchAction={fetchFaqs}
+            />
             <div className="card bg-base-100 shadow-md" style={{ minHeight: '600px', borderRadius: '5px' }}>
                 <div className="card-body">
                     <div className="grid grid-cols-2 gap-4">
@@ -55,8 +68,8 @@ const AdminFaq = () => {
                                     <div className="card-body p-1">
                                         <div className="flex flex-nowrap">
                                             <div className="basis-3/4">
-                                                <b>{faq.question}</b>
-                                                <p className='ml-5'>{faq.answer}</p>
+                                                <b>{parse(faq.question?.toString().substring(0, 130))}</b>
+                                                <p className='ml-5'>{parse(faq.answer?.toString().substring(0, 170))}</p>
                                             </div>
                                             <div className="basis-1/4 flex items-center justify-end">
                                                 {
@@ -75,7 +88,10 @@ const AdminFaq = () => {
                                                     </Link>
 
                                                     <button className="btn btn-sm">
-                                                        <TrashIcon className="h-4 w-4 text-error" />
+                                                        <TrashIcon
+                                                            className="h-4 w-4 text-error"
+                                                            onClick={() => handleShowConfirmModal(faq)}
+                                                        />
                                                     </button>
                                                 </div>
                                             </div>
@@ -85,7 +101,9 @@ const AdminFaq = () => {
                             ))
 
                         ) : (
-                            <p>No Faq</p>
+                            <div className="text-center">
+                                <p className="text-error">No record found.</p>
+                            </div>
                         )}
                     </div>
                 </div>
